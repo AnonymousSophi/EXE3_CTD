fac93d10b0b0d291c2f164e1006f8405e4a46a54
AnonymousSoph <149272712+AnonymousSophi@users.noreply.github.com>
2025-10-27 21:39:45 -0300

    Update usertop.vhd

--- usertop.vhd
+++ usertop.vhd
@@ -14,29 +14,25 @@ end usertop;
 
 
 architecture circuito_exe3 of usertop is
+signal N, S: std_logic_vector(3 downto 0); -- Sinais p/ entrada e saída do registrador
+signal CLK_STEP: std_logic;
 
+------------ COMPONENTES ------------
 
-component mux is 
-port (sel: in std_logic_vector(1 downto 0);
-	F1,F2,F3,F4: in std_logic_vector(3 downto 0);
-	F: out std_logic_vector(3 downto 0)
-    );
+component FSM_Conta is 
+port(clock: in std_logic;
+     reset: in std_logic;
+     contagem: out std_logic_vector(3 downto 0)
+     );
 end component;
 
-component Registrador is 
+component reg_4bits is 
 port (CLK:   in  std_logic;
 	  RST:   in  std_logic;
-	  Enter:   in  std_logic;
-      Q: out std_logic_vector(3 downto 0)
+	  N:   in  std_logic_vector(3 downto 0);
+      S: out std_logic_vector(3 downto 0)
       ); 
-end component;
-
-component somador is
-port (X: in std_logic_vector(3 downto 0);
-    Y: in std_logic_vector(3 downto 0);
-    S: out std_logic_vector(4 downto 0)
-    );
-end component;
+end component; 
 
 component decod7seg is
 port(C: in std_logic_vector(3 downto 0);
@@ -47,6 +43,23 @@ end component;
 
 begin
 
+CLK_STEP <= not KEY(1);
+
+LEDR(3 downto 0) <= N;
+LEDR(17 downto 4) <= (others => '0'); -- Desliga os outros LEDs que não estão em uso
+
+HEX1 <= (others => '1'); -- Desliga os outros HEX que não estão em uso
+HEX2 <= (others => '1'); 
+HEX3 <= (others => '1');
+HEX4 <= (others => '1'); 
+HEX5 <= (others => '1'); 
+HEX6 <= (others => '1'); 
+HEX7 <= (others => '1');
+
+
+FSM0: FSM_Conta port map (CLK_STEP, KEY(0), N); 
+REG0: reg_4bits port map (CLK_STEP, KEY(0), N, S);
+DEC0: decod7seg port map (S, HEX0);
 
 
 end circuito_exe3;
